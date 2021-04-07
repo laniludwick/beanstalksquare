6822cb1d97e23beb685765288c1f23c9ab26f584
Glory <lani.ludwick@gmail.com>
2021-04-07 16:49:22 +0000

    Add minor styling fixes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
-
+import './App.css';
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import GlobalNavigationBar from './GlobalNavigationBar';
