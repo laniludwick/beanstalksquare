f4a10f19d8326f1fdae5eac5a9d60aec9c9d9929
Glory <lani.ludwick@gmail.com>
2021-04-09 20:36:27 +0000

    Switch debug mode to false

--- server.py
+++ server.py
@@ -457,4 +457,4 @@ def send_stock_sms_to_teacher(teacher_id):
 
 if __name__ == '__main__':
     connect_to_db(app)
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=False)
